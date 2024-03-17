dd66321cab9f783bdf1ee5bf9cdfa9951cc8f936
Blake Fromkin <blakefromkin@gmail.com>
2024-03-17 15:01:01 -0400

    feat: lambda now adds an id property to the event payload via uuid

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,37 +1,40 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import { Client } from 'pg';
-import * as AWS from 'aws-sdk';
+import { Handler } from "aws-lambda";
+import { Client } from "pg";
+import { v4 as uuidv4 } from "uuid";
+import * as AWS from "aws-sdk";
 const secretsManager = new AWS.SecretsManager();
 
-export const handler: APIGatewayProxyHandler = async (event) => {
+export const handler: Handler = async event => {
   if (
     !event.body ||
-    typeof event.body !== 'string' ||
+    typeof event.body !== "string" ||
     JSON.parse(event.body).name === undefined
   ) {
     return {
       statusCode: 400,
-      headers: { 'Content-Type': 'text/plain' },
-      body: 'Invalid event request.',
+      headers: { "Content-Type": "text/plain" },
+      body: "Invalid event request.",
     };
   }
-
-  const eventBody = JSON.parse(event.body);
 
   const secretArn = process.env.RDS_SECRET_ARN;
   let secretValue;
   try {
     const data = await secretsManager
-      .getSecretValue({ SecretId: secretArn || '' })
+      .getSecretValue({ SecretId: secretArn || "" })
       .promise();
-    secretValue = JSON.parse(data.SecretString || '');
+    secretValue = JSON.parse(data.SecretString || "");
     if (!secretValue) {
-      throw new Error('Error retrieving secret.');
+      throw new Error("Error retrieving secret.");
     }
   } catch (err) {
     console.log(err);
     throw err as Error;
   }
+
+  const eventPayload = JSON.parse(event.body);
+  const eventId = uuidv4();
+  eventPayload.id = eventId;
 
   const client = new Client({
     host: process.env.RDS_PROXY_URL,
@@ -48,20 +51,20 @@ export const handler: APIGatewayProxyHandler = async (event) => {
     await client.connect();
     await client.query(
       `SELECT graphile_worker.add_job('process_event', $1,'event_processing_queue');`,
-      [eventBody]
+      [eventPayload]
     );
 
     return {
       statusCode: 200,
-      headers: { 'Content-Type': 'text/plain' },
-      body: 'Event processed.',
+      headers: { "Content-Type": "text/plain" },
+      body: "Event processed.",
     };
   } catch (err) {
     console.log(err);
     return {
       statusCode: 500,
-      headers: { 'Content-Type': 'text/plain' },
-      body: 'Error processing event.',
+      headers: { "Content-Type": "text/plain" },
+      body: "Error processing event.",
     };
   } finally {
     client.end();
