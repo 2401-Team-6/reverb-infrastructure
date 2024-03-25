752364bc4718feb8e50a4bceb0cb712bd4374353
Christopher Perkins <Christopher.Perkins@null.net>
2024-03-25 14:48:46 -0400

    fix: API Key now works for POST /events

--- lib/api/apiGatewayConstruct.ts
+++ lib/api/apiGatewayConstruct.ts
@@ -18,39 +18,33 @@ export default class ApiConstruct extends Construct {
         allowOrigins: api.Cors.ALL_ORIGINS,
         allowMethods: ["GET", "POST"],
       },
+      apiKeySourceType: api.ApiKeySourceType.HEADER,
     });
 
-    const apiDeployment = new api.Deployment(this, "ApiDeployment", {
-      api: this.apigw,
-    });
-
-    const apiStage = new api.Stage(this, "ApiStage", {
-      deployment: apiDeployment,
-      stageName: "reverb-prod-stage",
-    });
-
-    const apiKey = new api.ApiKey(this, "reverb-api-key", {
-      enabled: true,
-    });
+    const apiKey = new api.ApiKey(this, "reverb-api-key");
 
     const apiUsagePlan = new api.UsagePlan(this, "usage-plan", {
       name: "reverb-api-usage-plan",
+      apiStages: [
+        {
+          api: this.apigw,
+          stage: this.apigw.deploymentStage,
+        },
+      ],
       throttle: {
         burstLimit: 1000,
         rateLimit: 100,
       },
     });
     apiUsagePlan.addApiKey(apiKey);
-    apiUsagePlan.addApiStage({
-      api: this.apigw,
-      stage: apiStage,
-    });
 
     // Set lambda routes
     const eventsResource = this.apigw.root.addResource("events");
     const webhooksResource = this.apigw.root.addResource("webhooks");
     const lambdaIntegration = new api.LambdaIntegration(props.eventsLambda);
-    eventsResource.addMethod("POST", lambdaIntegration);
+    eventsResource.addMethod("POST", lambdaIntegration, {
+      apiKeyRequired: true,
+    });
     webhooksResource.addMethod("POST", lambdaIntegration);
   }
 }
