c94f6335d6be19da977a2b7fc782566c3bf312f5
Christopher Perkins <Christopher.Perkins@null.net>
2024-04-03 00:17:34 -0400

    chore: Removed error throw for functions env

--- utils/processEnvironment.ts
+++ utils/processEnvironment.ts
@@ -1,12 +1,6 @@
 import { config } from "dotenv";
 config();
 
-if (!process.env.FUNCTIONS_SERVER_IMAGE) {
-  throw new Error(
-    "Please provide your function server image repo in an environmental variable: FUNCTION_SERVER_IMAGE"
-  );
-}
-
 export const FUNCTIONS_URI =
   process.env.FUNCTIONS_SERVER_IMAGE || "quasari/dummy";
 export const WORKERS_URI =
